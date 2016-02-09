58b0fa7959985aa2caaa26537fc42c91d2eeb49e
André-Patrick Bubel <code@andre-bubel.de>
2016-02-09 06:33:04 +0000

    Init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+  long_description = "\n".join(f.readlines())
+
+setup(name='pyqrtray',
+  version='0.0.1',
+  description=u"",
+  long_description=long_description,
+  classifiers=[],
+  keywords='',
+  author=u"André-Patrick Bubel",
+  author_email=u"code@andre-bubel.de",
+  url='https://github.com/mapbox/pyskel',
+  license='GPL-3+',
+)
---
+++ src/main.py
@@ -0,0 +1,11 @@
+class Main:
+
+    def run(self):
+        pass
+
+
+def main():
+    Main().run()
+
+if __name__ == "__main__":
+    main()
