3b5c23890efd5581d6dde8980f4c5223e7fe2943
Andre-Patrick Bubel <code@andre-bubel.de>
2016-03-02 22:09:42 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
+# -*- coding: utf8 -*-
 from setuptools import setup
 
 with open("README.md", "r") as f:
     long_description = "\n".join(f.readlines())
 
-setup(name='pyqrtray',
+setup(name='qrtray',
       version='0.0.1',
-      description=u"",
+      description=u"Shows the current clipboard content as a QR code",
       long_description=long_description,
       classifiers=[],
       keywords='',
       author=u"André-Patrick Bubel",
       author_email=u"code@andre-bubel.de",
-      url='https://github.com/Moredread/pyqrtray',
+      url='https://github.com/Moredread/qrtray',
       license='GPL-3+',
       install_requires=[
           "pygobject",
